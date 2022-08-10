e30494399dda93a7c641b3603be7dc9d21ef3164
Stephanie Salgado <83602099+OnlyXSteph@users.noreply.github.com>
2022-08-10 10:17:01 -0500

    ready to deploy

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use('/', route);
     
 mongoose.connect("mongodb+srv://ssalgado:IXNbbBKgPAESTQ0k@cluster0.2kqty9i.mongodb.net/portfolioDB", {useNewURLParser: true, useUnifiedTopology: true})
 .then(() => {
-    app.listen(process.env.PORT || 3000, () => {
-        console.log("Server is running on 3000")
+    app.listen(process.env.PORT || 5000, () => {
+        console.log("Server is running on 5000")
     })
 })
 
